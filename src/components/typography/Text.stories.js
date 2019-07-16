176a69535bf68074f8559af979790ce04d29e401
Jeff <rustyangel@gmail.com>
2019-07-16 15:31:37 -0600

    Add component styles

--- src/components/typography/Text.stories.js
+++ src/components/typography/Text.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Text from '../src/Text';
+import Text from './Text';
 
 storiesOf('Text', module).add('with text', () => (
   <Text>Here's a text component</Text>
