a4ac005629998dee0c1e77b2c1b01ef946613ccf
Jeff <rustyangel@gmail.com>
2019-07-25 10:05:33 -0600

    Add Button prop types & custom styles

---
+++ .stylelintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@showbie/stylelint-config'],
+};
--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,6 +1,22 @@
+import './Button.scss';
+
 import React from 'react';
 
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
 export default class Button extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+    a11yLabel: PropTypes.string,
+    type: PropTypes.oneOf('button', 'submit'),
+    appearance: PropTypes.oneOf('default', 'primary', 'text'),
+    size: PropTypes.string,
+    isDisabled: PropTypes.bool,
+    isRunning: PropTypes.bool,
+    onClick: PropTypes.func,
+  };
+
   static defaultProps = {
     type: 'button',
     isDisabled: false,
@@ -11,16 +27,21 @@ export default class Button extends React.Component {
       children,
       a11yLabel,
       type,
-      // appearance,
+      appearance,
       // size,
       isDisabled,
       // isRunning,
       // onClick,
     } = this.props;
+    let buttonClass = classNames({
+      button: true,
+      'button--primary': appearance === 'primary',
+      'button--text': appearance === 'text',
+    });
 
     return (
       <button
-        className=""
+        className={buttonClass}
         aria-label={a11yLabel}
         type={type}
         disabled={isDisabled}
