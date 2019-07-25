5d95592ea861794a8bec7cb3fddf750ba307e365
Jeff <rustyangel@gmail.com>
2019-07-25 14:29:42 -0600

    Add Montserrat & more button styles

--- src/components/buttons/Button.stories.js
+++ src/components/buttons/Button.stories.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
-storiesOf('Button', module).add('examples', () => (
-  <>
-    <Button>Button</Button>
-    <Button isDisabled={true}>Button</Button>
-    <Button a11yLabel="Helpful text">Button</Button>
-  </>
-));
+storiesOf('Components|Buttons', module)
+  .add('basic', () => (
+    <div>
+      <div>
+        <Button a11yLabel="Helpful text">Button</Button>
+        <Button isDisabled={true}>Disabled</Button>
+      </div>
+      <div>
+        <Button appearance="primary">Primary</Button>
+        <Button appearance="primary" isDisabled={true}>
+          Disabled
+        </Button>
+      </div>
+    </div>
+  ))
+  .add('grouped', () => (
+    <>
+      <Button appearance="text">Primary</Button>
+    </>
+  ));
