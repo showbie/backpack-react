1a1055324fe26fa7dfa8aa9e19c00a7cd42172eb
Jeff <rustyangel@gmail.com>
2019-07-29 14:58:08 -0600

    Try out some storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+// import { DocsPage } from '@storybook/addon-docs/blocks';
+import { withInfo } from '@storybook/addon-info';
+import { addDecorator, addParameters, configure, load } from '@storybook/react';
+
+// addParameters({ docs: DocsPage });
+
+/**
+ * `withInfo` must be the first decorator.
+ */
+addDecorator(withInfo({ inline: true }));
+addDecorator(withA11y);
+
+// load(require.context('../src/components', true, /\.stories\.js$/), module);
 
 function loadStories() {
   let req = require.context('../src/components', true, /\.stories\.js$/);
