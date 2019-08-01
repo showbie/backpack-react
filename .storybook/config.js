0c150305b2a17272393e94a136eadac2953dc80f
Jeff <rustyangel@gmail.com>
2019-08-01 16:29:37 -0600

    Add default styles and disable addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { withA11y } from '@storybook/addon-a11y';
 // import { DocsPage } from '@storybook/addon-docs/blocks';
 import { withInfo } from '@storybook/addon-info';
@@ -11,6 +13,7 @@ addParameters({
       brandTitle: 'Backpack',
       gridCellSize: 12,
     }),
+    sortStoriesByKind: true,
   },
   // docs: DocsPage
 });
@@ -18,8 +21,9 @@ addParameters({
 /**
  * `withInfo` must be the first decorator.
  */
-addDecorator(withInfo({ inline: true }));
+// addDecorator(withInfo({ inline: true }));
 addDecorator(withA11y);
+addDecorator((story) => <div style={{ padding: '2rem' }}>{story()}</div>);
 
 // load(require.context('../src/components', true, /\.stories\.js$/), module);
 
